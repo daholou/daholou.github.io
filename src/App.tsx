2d265e3d0d11ddb792ea9077df0054547d206a1a
daholou <hetsuvi.aholou@gmail.com>
2023-05-13 12:07:16 +0200

    texte

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          Coucou, voilà du texte supplémentaire :)
         </p>
         <a
           className="App-link"
